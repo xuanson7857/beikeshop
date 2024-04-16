2222abf289b97d8339be2e464cbe3cf02734e15f
xuanson7857 <satomeo1102@gmail.com>
2024-04-16 09:29:09 +0700

    sd

---
+++ resources/lang/zh_cn/shop/account/addresses.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * account.php
+ *
+ * @copyright  2022 beikeshop.com - All Rights Reserved
+ * @link       https://beikeshop.com
+ * @author     Edward Yang <yangjin@guangda.work>
+ * @created    2022-08-04 10:59:15
+ * @modified   2022-08-04 10:59:15
+ */
+
+return [
+    'index' => 'Địa chỉ',
+    'add_address' => 'Thêm địa chỉ mới',
+    'default_address' => 'Mặc định',
+    'delete' => 'Xóa',
+    'edit' => 'Chỉnh sửa',
+    'enter_name' => 'Vui lòng nhập tên của bạn',
+    'enter_phone' => 'Vui lòng nhập số điện thoại của bạn',
+    'enter_address' => 'Vui lòng nhập địa chỉ chi tiết 1',
+    'select_province' => 'Vui lòng chọn tỉnh/thành phố',
+    'enter_city' => 'Quận/Huyện',
+    'confirm_delete' => 'Bạn có chắc chắn muốn xóa địa chỉ này?',
+    'hint' => 'Gợi ý',
+    'check_form' => 'Vui lòng kiểm tra lại thông tin đã nhập',
+];
